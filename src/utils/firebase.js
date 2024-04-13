f61d85cf81160bc7db8e62f574d93899f448c4db
Sukaran Gulati <83487387+gulatisukaran@users.noreply.github.com>
2024-04-13 14:28:03 +0530

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,7 +5,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBxAESiovOAQlv_jUDE95uDwL1yboxIOkc",
+    apiKey: "your_firebase_api_key",
     authDomain: "valentine-cbe23.firebaseapp.com",
     projectId: "valentine-cbe23",
     storageBucket: "valentine-cbe23.appspot.com",
